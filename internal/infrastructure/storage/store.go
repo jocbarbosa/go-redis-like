179e362113037f67dc5d1c5f7b07164a7a18a2b6
Joao Barbosa <joaao@outlook.com>
2026-09-03 15:15:42 +0100

    implement the keyStoreRepository interface

--- internal/infrastructure/storage/store.go
+++ internal/infrastructure/storage/store.go
@@ -2,6 +2,7 @@ package storage
 
 import (
 	"context"
+	"path/filepath"
 	"sync"
 	"time"
 
@@ -21,6 +22,8 @@ func NewStore() repository.KeyValueRepository {
 		stopCleanup: make(chan struct{}),
 	}
 }
+
+// Set sets a new item in the mapping
 func (s *Store) Set(ctx context.Context, key, value string) {
 	if ctx.Err() != nil {
 		return
@@ -36,6 +39,7 @@ func (s *Store) Set(ctx context.Context, key, value string) {
 
 }
 
+// Get returns an item when is available based on the key
 func (s *Store) Get(ctx context.Context, key string) (string, bool) {
 	if ctx.Err() != nil {
 		return "", false
@@ -57,6 +61,7 @@ func (s *Store) Get(ctx context.Context, key string) (string, bool) {
 
 }
 
+// Del deletes an item based on a given key
 func (s *Store) Del(ctx context.Context, key string) int {
 	if ctx.Err() != nil {
 		return 0
@@ -69,6 +74,7 @@ func (s *Store) Del(ctx context.Context, key string) int {
 	return 1
 }
 
+// Expire sets an expiration TTL for an item key
 func (s *Store) Expire(ctx context.Context, key string, seconds int) bool {
 	if ctx.Err() != nil {
 		return false
@@ -90,6 +96,7 @@ func (s *Store) Expire(ctx context.Context, key string, seconds int) bool {
 
 }
 
+// TTL sets a TTL to a given item key
 func (s *Store) TTL(ctx context.Context, key string) int64 {
 	if ctx.Err() != nil {
 		return -1
@@ -117,22 +124,122 @@ func (s *Store) TTL(ctx context.Context, key string) int64 {
 
 }
 
+// Persist removes TTL and transform an item to persistent
 func (s *Store) Persist(ctx context.Context, key string) bool {
-	return false
+	if ctx.Err() != nil {
+		return false
+	}
+
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	item, exists := s.data[key]
+	if !exists {
+		return false
+	}
+
+	item.ExpiresAt = nil
+
+	return true
+
 }
 
+// Keys return all keys that matches a given pattern
 func (s *Store) Keys(ctx context.Context, pattern string) []string {
-	return []string{}
+	if ctx.Err() != nil {
+		return []string{}
+	}
+
+	s.mu.RLock()
+	defer s.mu.Unlock()
+
+	now := time.Now().Unix()
+	var matches []string
+
+	for key, item := range s.data {
+		if item.IsExpired(now) {
+			continue
+		}
+
+		if matchPattern(key, pattern) {
+			matches = append(matches, key)
+		}
+	}
+
+	return matches
+
 }
 
+// Exists returns if a given key exists
 func (s *Store) Exists(ctx context.Context, key string) bool {
-	return false
+	if ctx.Err() != nil {
+		return false
+	}
+
+	s.mu.RLock()
+	defer s.mu.Unlock()
+
+	item, exists := s.data[key]
+	if !exists {
+		return false
+	}
+
+	return !item.IsExpired(time.Now().Unix())
 }
 
+// Size returns the number of all items
+// this ignore if the item is expired or not
 func (s *Store) Size(ctx context.Context) int {
-	return 0
+	if ctx.Err() != nil {
+		return 0
+	}
+
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	return len(s.data)
 }
 
-func (s *Store) StartCleanup(intervalMs int64) {}
+// StartCleanup cleans up all expired keys
+func (s *Store) StartCleanup(ctx context.Context, intervalMs int64) {
+	interval := time.Duration(intervalMs) * time.Millisecond
+	go func() {
+		ticker := time.NewTicker(interval)
+		defer ticker.Stop()
 
-func (s *Store) StopCleanup() {}
+		for {
+			select {
+			case <-ticker.C:
+				now := time.Now().Unix()
+				for k, v := range s.data {
+					if v.IsExpired(now) {
+						s.Del(ctx, k)
+					}
+				}
+			case <-ctx.Done():
+				return
+			case <-s.stopCleanup:
+				return
+			}
+		}
+
+	}()
+}
+
+func (s *Store) StopCleanup() {
+	close(s.stopCleanup)
+}
+
+func matchPattern(key, pattern string) bool {
+	if pattern == "*" {
+		return true
+	}
+
+	matched, err := filepath.Match(pattern, key)
+	if err != nil {
+		return false
+	}
+
+	return matched
+
+}
