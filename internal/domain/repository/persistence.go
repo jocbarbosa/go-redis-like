2f765f4e81d746573f3ac3916fe71ad99c9f7528
Joao Barbosa <joaao@outlook.com>
2026-09-15 19:23:59 +0100

    add aof support

--- internal/domain/repository/persistence.go
+++ internal/domain/repository/persistence.go
@@ -5,5 +5,5 @@ import "context"
 type PersistenceRepository interface {
 	Append(ctx context.Context, cmd string, args []string) error
 	Replay(ctx context.Context, store KeyValueRepository) error
-	Close()
+	Close() error
 }
--- internal/infrastructure/persistence/aof.go
+++ internal/infrastructure/persistence/aof.go
@@ -1,12 +1,15 @@
 package persistence
 
 import (
+	"bufio"
 	"context"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 	"sync"
 
+	"github.com/jocbarbosa/go-redis-like/internal/domain/command"
 	"github.com/jocbarbosa/go-redis-like/internal/domain/repository"
 )
 
@@ -54,7 +57,86 @@ func (a *AOF) Append(ctx context.Context, cmd string, args []string) error {
 }
 
 func (a *AOF) Replay(ctx context.Context, store repository.KeyValueRepository) error {
+	if ctx.Err() != nil {
+		return ctx.Err()
+	}
+
+	file, err := os.Open(a.filepath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil
+		}
+
+		return err
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		if ctx.Err() != nil {
+			return ctx.Err()
+		}
+
+		line := strings.TrimSpace(scanner.Text())
+		if line == "" {
+			continue
+		}
+
+		fields := strings.Fields(line)
+		if len(fields) == 0 {
+			continue
+		}
+
+		cmd := strings.ToUpper(fields[0])
+		args := fields[1:]
+
+		switch command.Type(cmd) {
+		case command.SET:
+			if len(args) < 2 {
+				continue
+			}
+
+			key := args[0]
+			value := strings.Join(args[1:], " ")
+			store.Set(ctx, key, value)
+		case command.EXPIRE:
+			if len(args) < 2 {
+				continue
+			}
+
+			key := args[0]
+			seconds, err := strconv.Atoi(args[1])
+			if err != nil {
+				continue
+			}
+
+			store.Expire(ctx, key, seconds)
+		case command.DEL:
+			if len(args) < 1 {
+				continue
+			}
+
+			store.Del(ctx, args[0])
+		default:
+		}
+
+	}
+
+	if scanner.Err() != nil {
+		return fmt.Errorf("error reading AOF file %w", err)
+	}
+
 	return nil
 }
 
-func (a *AOF) Close() {}
+func (a *AOF) Close() error {
+	a.mu.Lock()
+	defer a.mu.Unlock()
+
+	if a.file != nil {
+		a.file.Close()
+	}
+
+	return nil
+}
